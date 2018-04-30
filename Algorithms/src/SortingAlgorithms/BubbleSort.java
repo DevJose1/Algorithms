6eb6d2ac3f204f4c3c6674e89cdac4ce86b2a924
atiq <joseph1atiq@gmail.com>
2018-04-30 17:30:13 +0200

    Bubble Sort with Java

--- Algorithms/src/SortingAlgorithms/BubbleSort.java
+++ Algorithms/src/SortingAlgorithms/BubbleSort.java
@@ -1,5 +1,29 @@
 package SortingAlgorithms;
-
+/*This algorithm is used to sort a list of elements by comparing 
+ * two adjacent elements. We swapped If there is no order, Then 
+we continue until An ordered list and no more swapping is needed */
 public class BubbleSort {
+	
+	public static void bubbleSort(int[] tab) {
+		boolean swapping = true;
+		for (int i=0; i< tab.length-1; i++) {
+			swapping = false;
+			for(int j=0; j<tab.length-i-1; j++) {
+				if (tab[j]>tab[j+1]) {
+					int temp = tab[j];
+					tab[j] = tab[j+1];
+					tab[j+1]= temp;
+					swapping = true;
+				}
+				
+			}
+			if (swapping==false) {
+				break;
+			}
+			
+			
+		}
+		
+	}
 
 }
