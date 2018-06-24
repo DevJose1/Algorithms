82169ef47fb5827f87dfb583c7e749a522a257be
atiq <joseph1atiq@gmail.com>
2018-06-25 00:32:25 +0200

    AppleandOrange!!!

---
+++ Algorithms/src/AppleandOrange.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+import javax.swing.text.Position;
+
+public class AppleandOrange {
+	
+static void countApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges) {
+       
+	 // Complete the countApplesAndOranges function below.
+    static void countApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges) {
+        int countApple=0;
+        int pos=0;
+        int countOrange=0;
+        for (int i=0; i<apples.length; i++){
+    
+            pos = apples[i]+a;
+            if(pos>=s && pos<= t){
+                countApple++;
+            }         
+        }
+        for(int i=0; i<oranges.length; i++){
+            pos= oranges[i]+b;
+            
+            if(pos>=s && pos<= t){
+                countOrange++;
+}
+            
+            
+        }
+        System.out.println(countApple);
+        System.out.println(countOrange);
+
+}
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String[] st = scanner.nextLine().split(" ");
+
+        int s = Integer.parseInt(st[0]);
+
+        int t = Integer.parseInt(st[1]);
+
+        String[] ab = scanner.nextLine().split(" ");
+
+        int a = Integer.parseInt(ab[0]);
+
+        int b = Integer.parseInt(ab[1]);
+
+        String[] mn = scanner.nextLine().split(" ");
+
+        int m = Integer.parseInt(mn[0]);
+
+        int n = Integer.parseInt(mn[1]);
+
+        int[] apples = new int[m];
+
+        String[] applesItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < m; i++) {
+            int applesItem = Integer.parseInt(applesItems[i]);
+            apples[i] = applesItem;
+        }
+
+        int[] oranges = new int[n];
+
+        String[] orangesItems = scanner.nextLine()
+    
